ed3ac7c2680b49d7def1d99259247ac0ea31dd77
xiaoxiong05 <491421915@qq.com>
2019-11-12 10:00:14 +0800

    add index and bootstrap

--- src/main/java/com/example/lianxi2/controller/IndexController.java
+++ src/main/java/com/example/lianxi2/controller/IndexController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-//@RequestMapping("/hello")
-public class UserController {
+//@RequestMapping("/")
+public class IndexController {
 
-    @RequestMapping("/hello")
-    public String getGreen(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @RequestMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
